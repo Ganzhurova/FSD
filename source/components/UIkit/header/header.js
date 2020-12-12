17a095d918ccdab45c533d13306a3badd0e3e4b9
Darima Ganzhurova <DGanzurova@gmail.com>
2020-12-12 16:04:30 +0900

    Удалены ненужные файлы

--- source/components/UIkit/header/header.js
+++
@@ -1,9 +0,0 @@
-import './img/icon-logo.svg';
-import './header.scss';
-
-// const svg = {
-//   path: './img/**/*.svg',
-//   name: 'assets/svg/[hash].logos.svg',
-// };
-//
-// require('webpack-svgstore-plugin/src/helpers/svgxhr')(svg);
