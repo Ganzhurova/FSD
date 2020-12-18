28b957d221be63ded8440b5be70384a305679029
Darima Ganzhurova <DGanzurova@gmail.com>
2020-12-18 19:12:25 +0900

    Добавлен inputmask

--- source/components/input/input.js
+++ source/components/input/input.js
@@ -1,1 +1,16 @@
 import './input.scss';
+
+import Inputmask from 'inputmask';
+
+const dateMask = {
+  alias: 'datetime',
+  inputFormat: 'dd.mm.yyyy',
+  placeholder: '__.__.____',
+};
+
+function addMask(selector, maskConfig) {
+  const im = new Inputmask(maskConfig);
+  im.mask(document.querySelectorAll(selector));
+}
+
+addMask('.js-input--date-mask', dateMask);
