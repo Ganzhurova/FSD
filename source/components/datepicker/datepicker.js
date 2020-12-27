48edef6902028032e47a1fccb4a55e144fa3c172
Darima Ganzhurova <DGanzurova@gmail.com>
2020-12-27 17:51:32 +0900

    Удалены настройки для календаря в uikit

--- source/components/datepicker/datepicker.js
+++ source/components/datepicker/datepicker.js
@@ -7,25 +7,8 @@ $('.datepicker-here').datepicker({
   },
   prevHtml: '<i class="material-icons">arrow_back</i>',
   nextHtml: '<i class="material-icons">arrow_forward</i>',
-  range: true,
   multipleDatesSeparator: ' - ',
   clearButton: true,
   todayButton: true,
-});
-
-const uikitCalendar = $('.uikit-calendar')
-  .datepicker()
-  .data('datepicker');
-
-uikitCalendar.date = new Date(2019, 7, 8);
-
-uikitCalendar.update({
-  onRenderCell(date, cellType) {
-    if (cellType === 'day' && date.getDate() === 8) {
-      return {
-        classes: '-current-',
-      };
-    }
-    return false;
-  },
+  minDate: new Date(),
 });
