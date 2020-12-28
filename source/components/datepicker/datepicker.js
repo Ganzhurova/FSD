09d69f80ecab3c7151c6f79d0ec8da7d11d18049
Darima Ganzhurova <DGanzurova@gmail.com>
2020-12-28 19:56:49 +0900

    Добавлена локализация для русского языка

--- source/components/datepicker/datepicker.js
+++ source/components/datepicker/datepicker.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import 'air-datepicker';
 import './datepicker.scss';
 
 $('.datepicker-here').datepicker({
@@ -12,3 +13,50 @@ $('.datepicker-here').datepicker({
   todayButton: true,
   minDate: new Date(),
 });
+
+$.fn.datepicker.language.ru = {
+  days: [
+    'Воскресенье',
+    'Понедельник',
+    'Вторник',
+    'Среда',
+    'Четверг',
+    'Пятница',
+    'Суббота',
+  ],
+  daysShort: ['Вос', 'Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб'],
+  daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+  months: [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
+  ],
+  monthsShort: [
+    'янв',
+    'фев',
+    'мар',
+    'апр',
+    'май',
+    'июн',
+    'июл',
+    'авг',
+    'сен',
+    'окт',
+    'ноя',
+    'дек',
+  ],
+  today: 'Сегодня',
+  clear: 'Очистить',
+  dateFormat: 'dd.mm.yyyy',
+  timeFormat: 'hh:ii',
+  firstDay: 1,
+};
