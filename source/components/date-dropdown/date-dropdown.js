513e330b3507548ee6a7ed86cd0bfae54bd1f957
Darima Ganzhurova <DGanzurova@gmail.com>
2021-02-03 12:19:33 +0900

    Компонент переписаню Реализована возможность вывода дат из календаря в два инпута

--- source/components/date-dropdown/date-dropdown.js
+++ source/components/date-dropdown/date-dropdown.js
@@ -2,17 +2,71 @@ import $ from 'jquery';
 import 'air-datepicker';
 import './date-dropdown.scss';
 
-$('.datepicker-here').datepicker({
+const rangeInitClass = '.js-range-date-dropdown';
+const filterInitClass = '.js-filter-date-dropdown';
+const rangeWrapClass = '.date-dropdown--range';
+const rangeFromClass = '.js-date-dropdown-from';
+const rangeToClass = '.js-date-dropdown-to';
+
+/* Базовая конфигурация datepicker */
+const baseConfig = {
   navTitles: {
     days: 'MM yyyy',
   },
   prevHtml: '<i class="material-icons">arrow_back</i>',
   nextHtml: '<i class="material-icons">arrow_forward</i>',
   multipleDatesSeparator: ' - ',
+  range: true,
   clearButton: true,
-  todayButton: true,
   minDate: new Date(),
-});
+};
+
+/* Инициализация плагина datepicker */
+
+/* для фильтра */
+const filterConfig = { dateFormat: 'dd M' };
+
+$.extend(true, filterConfig, baseConfig);
+
+$(filterInitClass).datepicker(filterConfig);
+
+/* для диапазона дат (вывод в два инпута) */
+const rangeItems = $(rangeWrapClass);
+
+if (rangeItems.length > 0) {
+  rangeItems.each(function initRange() {
+    const item = $(this);
+    const dateFrom = item.find(rangeFromClass);
+    const dateTo = item.find(rangeToClass);
+    const rangeConfig = {
+      onSelect: date => {
+        const dates = date.split(' - ');
+        dateFrom.val(dates[0]);
+        dateTo.val(dates[1]);
+      },
+    };
+    $.extend(true, rangeConfig, baseConfig);
+    item.find(rangeInitClass).datepicker(rangeConfig);
+
+    const datepicker = item.find(rangeInitClass).data('datepicker');
+
+    dateFrom.click(() => {
+      datepicker.show();
+    });
+
+    dateTo.click(() => {
+      datepicker.show();
+    });
+  });
+}
+
+/* Добавление кнопки "Применить" */
+
+const buttonApply = $(
+  '<span class="datepicker--button datepicker--button-apply" data-action="apply">Применить</span>'
+);
+
+$('.datepicker--buttons').append(buttonApply);
 
 $.fn.datepicker.language.ru = {
   days: [
@@ -60,19 +114,3 @@ $.fn.datepicker.language.ru = {
   timeFormat: 'hh:ii',
   firstDay: 1,
 };
-
-const $start = $('.js-datepicker-start');
-const $end = $('.js-datepicker-end');
-
-$start.datepicker({
-  onSelect(fd, date) {
-    $end.data('datepicker').update('minDate', date);
-    $end.focus();
-  },
-});
-
-$end.datepicker({
-  onSelect(fd, date) {
-    $start.data('datepicker').update('maxDate', date);
-  },
-});
