6cf3330066e6460cba6d5b5d9c2159125d3ae32d
Darima Ganzhurova <DGanzurova@gmail.com>
2021-02-17 15:06:03 +0900

    Реализовано открытие/закрытие меню на js

--- source/components/navigation/navigation.js
+++ source/components/navigation/navigation.js
@@ -1,1 +1,48 @@
 import './navigation.scss';
+
+class Navigation {
+  constructor() {
+    this.openedClass = 'nav--opened';
+    this.closedClass = 'nav--closed';
+  }
+
+  init(el) {
+    this.navigation = el;
+    this.target = el.querySelector('.js-nav-toggle');
+
+    this.actions();
+  }
+
+  show() {
+    this.navigation.classList.remove(this.closedClass);
+    this.navigation.classList.add(this.openedClass);
+  }
+
+  close() {
+    this.navigation.classList.remove(this.openedClass);
+    this.navigation.classList.add(this.closedClass);
+  }
+
+  actions() {
+    this.target.addEventListener('click', () => {
+      if (this.navigation.classList.contains(this.closedClass)) {
+        this.show();
+      } else {
+        this.close();
+      }
+    });
+  }
+}
+
+function initNavigation() {
+  const nav = document.querySelector('.js-nav');
+
+  if (nav) {
+    const menu = new Navigation();
+    menu.init(nav);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  initNavigation();
+});
