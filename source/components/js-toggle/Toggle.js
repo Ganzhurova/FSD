51edcf8919b6f5d5da34b63001b1c9ebcec1647b
Darima Ganzhurova <DGanzurova@gmail.com>
2021-02-28 15:33:06 +0900

    Создана отдельная функция для открытия/закрытия эдементов, вынесена из компонента navigation

---
+++ source/components/js-toggle/Toggle.js
@@ -0,0 +1,53 @@
+class Toggle {
+  constructor() {
+    this.openMod = '--opened';
+    this.closeMod = '--closed';
+  }
+
+  init(el) {
+    this.toggle = el;
+    this.target = el.querySelector('.js-toggle');
+    this.className = el.dataset.name;
+    this.openedClass = `${this.className}${this.openMod}`;
+    this.closedClass = `${this.className}${this.closeMod}`;
+
+    this.actions();
+  }
+
+  show() {
+    this.toggle.classList.remove(this.closedClass);
+    this.toggle.classList.add(this.openedClass);
+  }
+
+  close() {
+    this.toggle.classList.remove(this.openedClass);
+    this.toggle.classList.add(this.closedClass);
+  }
+
+  actions() {
+    this.target.addEventListener('click', () => {
+      if (this.toggle.classList.contains(this.closedClass)) {
+        this.show();
+      } else {
+        this.close();
+      }
+    });
+  }
+}
+
+function initToggle() {
+  const blocks = document.querySelectorAll('.js-switchable');
+
+  if (blocks.length > 0) {
+    for (let i = 0; i < blocks.length; i += 1) {
+      const block = blocks[i];
+      const toggle = new Toggle();
+      toggle.init(block);
+      console.log(toggle);
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  initToggle();
+});
