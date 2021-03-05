20a3fddd32f95a655d579938a4c9ba8ac61be4d4
Darima Ganzhurova <DGanzurova@gmail.com>
2021-03-05 21:53:49 +0900

    Первичная настройка плагина paginationjs

--- source/components/pagination/pagination.js
+++ source/components/pagination/pagination.js
@@ -1,1 +1,28 @@
+import $ from 'jquery';
 import './pagination.scss';
+import 'paginationjs';
+
+import data from '../../pages/website/search/search';
+
+const roomTemplate = require('../room/template.pug');
+
+function template(rooms) {
+  let html = '';
+  $.each(rooms, function getHtml(index, room) {
+    const locals = {
+      room,
+    };
+    html += roomTemplate(locals);
+  });
+
+  return html;
+}
+
+$('#pagination-container').pagination({
+  dataSource: data,
+  locator: 'rooms',
+  callback(rooms) {
+    const html = template(rooms);
+    $('#js-data-container').html(html);
+  },
+});
