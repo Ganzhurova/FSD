2d3ac4c290525948873f7f8acff7cebef69eb86f
Darima Ganzhurova <DGanzurova@gmail.com>
2021-03-06 16:30:22 +0900

    Настройка плагина paginationjs

--- source/components/pagination/pagination.js
+++ source/components/pagination/pagination.js
@@ -18,11 +18,26 @@ function template(rooms) {
   return html;
 }
 
-$('#pagination-container').pagination({
-  dataSource: data,
-  locator: 'rooms',
-  callback(rooms) {
-    const html = template(rooms);
-    $('#js-data-container').html(html);
-  },
-});
+function initPagination() {
+  const container = $('#js-pagination-container');
+
+  if (container.length > 0) {
+    container.pagination({
+      dataSource: data,
+      locator: 'rooms',
+      autoHidePrevious: true,
+      autoHideNext: true,
+      pageSize: 12,
+      callback(rooms) {
+        const html = template(rooms);
+        $('#js-data-container').html(html);
+      },
+      prevText: '<span class="visually-hidden">Назад</span>',
+      nextText: '<span class="visually-hidden">Вперед</span>',
+      footer:
+        '<p class="pagination__footer">1 – 12 из 100+ вариантов аренды</p>',
+    });
+  }
+}
+
+initPagination();
