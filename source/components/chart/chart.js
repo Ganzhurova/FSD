043121f9d5d51d7aeef534e35d0b0d5658df3cad
Darima Ganzhurova <DGanzurova@gmail.com>
2021-03-21 22:41:51 +0900

    Черновой вариант функции для отрисовки сегментов диаграммы

--- source/components/chart/chart.js
+++ source/components/chart/chart.js
@@ -3,7 +3,13 @@ import declensionNouns from '../helper/declensionNouns';
 
 class Chart {
   constructor() {
-    this.impression = {};
+    this.data = {};
+    this.dataPercent = {};
+    this.total = 0;
+    this.circumference = 0;
+    this.cx = 60;
+    this.cy = 60;
+    this.angleOffset = -90;
     this.textForms = ['голос', 'голоса', 'голосов'];
   }
 
@@ -11,9 +17,12 @@ class Chart {
     this.chart = el;
     this.totalEl = el.querySelector('.js-total-number');
     this.totalTextEl = el.querySelector('.js-total-text');
+    this.radius = el.querySelector('.js-segment').getAttribute('r');
 
     this.getSegments();
     this.getTotal();
+    this.getCircumference();
+    this.getDasharrayVal();
     this.renderTotal();
   }
 
@@ -22,12 +31,12 @@ class Chart {
     segments.forEach(item => {
       const name = item.dataset.segment;
       const value = Number(item.dataset.total);
-      this.impression[name] = value;
+      this.data[name] = value;
     });
   }
 
   getTotal() {
-    this.total = Object.values(this.impression).reduce(
+    this.total = Object.values(this.data).reduce(
       (total, amount) => total + amount
     );
   }
@@ -35,6 +44,41 @@ class Chart {
   renderTotal() {
     this.totalEl.textContent = this.total;
     this.totalTextEl.textContent = declensionNouns(this.total, this.textForms);
+  }
+
+  getCircumference() {
+    this.circumference = 2 * Math.PI * this.radius;
+  }
+
+  getDasharrayVal() {
+    const segments = this.chart.querySelectorAll('[data-segment]');
+    // let offset = 0;
+
+    segments.forEach(item => {
+      const value = Number(item.dataset.total);
+      const percent = value / this.total;
+      // const dash = this.circumference * percent;
+      // const adjustedDash = dash - 2;
+      // const gap = this.circumference - dash;
+      // const attrVal = `${adjustedDash} ${gap}`;
+      const offset = this.circumference - this.circumference * percent;
+      const circle = item.querySelector('.js-segment');
+      const adjustedCircumference = this.circumference - 2;
+
+      circle.setAttribute('stroke-dasharray', adjustedCircumference);
+      circle.setAttribute('stroke-dashoffset', offset);
+      circle.setAttribute(
+        'transform',
+        `rotate(${this.angleOffset}, ${this.cx}, ${this.cy})`
+      );
+
+      this.angleOffset = percent * 360 + this.angleOffset;
+      // offset += dash;
+      //
+      // console.log(dash);
+      console.log(this.angleOffset);
+      // console.log(offset);
+    });
   }
 }
 
