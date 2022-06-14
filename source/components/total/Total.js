5e8e933acf9b20b6e425b5e0bd65214ab3cc50fb
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-14 21:36:16 +0800

    Компонент total вынесен в отдельный миксин

---
+++ source/components/total/Total.js
@@ -0,0 +1,56 @@
+import './total.scss';
+
+class Total {
+  constructor(el, parentInstance) {
+    this.parent = parentInstance;
+    this.el = el;
+    this.inputEl = this.el.querySelector('input');
+    this.value = +this.inputEl.value;
+
+    this.disableDecreaseButton();
+    this.addEventListeners();
+    this.giveValue();
+  }
+
+  setInputValue(number) {
+    this.inputEl.value = number;
+    this.disableDecreaseButton();
+  }
+
+  disableDecreaseButton() {
+    const decreaseButton = this.el.querySelector('[data-action = decrease]');
+    decreaseButton.disabled = this.value === 0;
+  }
+
+  giveValue() {
+    this.parent.updateItems(this.inputEl.name, this.inputEl.value);
+  }
+
+  decrease() {
+    this.value -= 1;
+    this.setInputValue(this.value);
+    this.giveValue();
+  }
+
+  increase() {
+    this.value += 1;
+    this.setInputValue(this.value);
+    this.giveValue();
+  }
+
+  handlerDataAction(evt) {
+    const { action } = evt.target.dataset;
+
+    if (action && this[action]) {
+      this[action]();
+    }
+  }
+
+  addEventListeners() {
+    this.handlerDataAction = this.handlerDataAction.bind(this);
+
+    this.el.addEventListener('click', this.handlerDataAction);
+  }
+}
+
+export default Total;
