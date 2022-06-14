795c4c8afc336cb1777054428ce07cfd8ada626d
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-14 22:18:58 +0800

    Added baseConfig

--- source/components/calendar/getDatepicker.js
+++ source/components/calendar/getDatepicker.js
@@ -1,9 +1,10 @@
 /* global $ */
 
 import 'air-datepicker';
+import baseConfig from './config/base-config';
 
 function getDatepicker(calendarEl) {
-  return $(calendarEl).datepicker().data('datepicker');
+  return $(calendarEl).datepicker(baseConfig).data('datepicker');
 }
 
 export default getDatepicker;
