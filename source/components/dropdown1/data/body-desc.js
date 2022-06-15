f43f3e48fee785d693bbfc459e6cdbdd3d6b863d
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-15 20:32:42 +0800

    Добавлены опции для меню удобства номера

--- source/components/dropdown1/data/body-desc.js
+++ source/components/dropdown1/data/body-desc.js
@@ -1,4 +1,8 @@
 const guestsList = require('./guests-list');
+const comfortList = require('./comfort-list');
+// const baseConfig = require('../../calendar/config/base-config');
+
+// console.log(baseConfig);
 
 const bodyDesc = {
   guests: {
@@ -14,7 +18,15 @@ const bodyDesc = {
     name: 'calendar',
     data: {
       bodySelector: '.js-calendar',
+      // config: baseConfig,
       control: true,
+    },
+  },
+  comfort: {
+    name: 'totalList',
+    data: {
+      bodySelector: '.js-total-list',
+      list: comfortList,
     },
   },
 };
