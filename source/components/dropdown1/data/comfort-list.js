faaf41485c47cb3ea843335af226598b6db46ace
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-15 20:33:17 +0800

    Создан список опций для меню удобства номера

---
+++ source/components/dropdown1/data/comfort-list.js
@@ -0,0 +1,18 @@
+const comfortList = {
+  bedroom: {
+    text: 'Спальни',
+    value: 2,
+    txtArr: ['спален', 'спальня', 'спальни'],
+  },
+  bed: {
+    text: 'Кровати',
+    value: 2,
+    txtArr: ['кроватей', 'кровать', 'кровати'],
+  },
+  bathroom: {
+    text: 'Ванные комнаты',
+    txtArr: ['ванных комнат', 'ванная комната', 'ванные комнаты'],
+  },
+};
+
+module.exports = comfortList;
