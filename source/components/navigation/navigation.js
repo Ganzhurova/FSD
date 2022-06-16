6246bcba49d5c599d857c2c8d3b3e5fecd399941
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-16 17:39:56 +0800

    Added initBaseDropdown

--- source/components/navigation/navigation.js
+++ source/components/navigation/navigation.js
@@ -1,1 +1,8 @@
 import './navigation.scss';
+import initBaseDropdowns from '../js/dropdown/initBaseDropdowns';
+
+initBaseDropdowns('.js-nav', {
+  targetSelector: '.js-toggle',
+  closeClass: 'nav--closed',
+  openClass: 'nav--opened',
+});
