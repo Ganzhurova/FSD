841fa008b9c774a6d71f41487b3140c531cd0d53
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-16 21:58:35 +0800

    Добавлено создание календаря

---
+++ source/pages/UIkit/cards/cards.js
@@ -0,0 +1,6 @@
+/* global $ */
+
+import 'air-datepicker';
+import baseConfig from '../../../components/calendar/config/base-config';
+
+$('.content__section--datepicker .js-datepicker').datepicker(baseConfig);
