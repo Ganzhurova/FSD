522a4c734c24ba67984e0064d7c1f1fcd810eb2e
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 12:57:40 +0800

    Рефакторинг

--- source/components/dropdown/initDropdowns.js
+++ source/components/dropdown/initDropdowns.js
@@ -3,8 +3,6 @@ import Dropdown from './Dropdown';
 function initDropdowns(selector) {
   const dropdowns = [...document.querySelectorAll(selector)];
   dropdowns.forEach((dropdown) => {
-    // const i = new Dropdown(dropdown);
-    // console.log(i);
     (() => new Dropdown(dropdown))();
   });
 }
