7063e7c2e4a0af496de3ec8daa6f24a2a301dbfe
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 16:34:29 +0800

    Создан класс Pagination

--- source/components/pagination/pagination.js
+++ source/components/pagination/pagination.js
@@ -1,93 +1,46 @@
-/* global $ */
-
-import './pagination.scss';
 import 'paginationjs';
-import initRatings from '../rating/rating';
+import paginationConfig from './paginationConfig';
+import getPaginationjsContainer from './getPaginationjsContainer';
 
-import data from '../../pages/website/search/search';
+class Pagination {
+  constructor(el, options) {
+    this.$el = getPaginationjsContainer(el);
+    this.options = options;
 
-const roomTemplate = require('../room/template.pug');
-
-function template(rooms) {
-  let html = '';
-  $.each(rooms, (index, room) => {
-    const locals = {
-      room,
-    };
-    html += roomTemplate(locals);
-  });
-
-  return html;
-}
-
-function initSlider() {
-  $('.js-slider').slick({
-    dots: true,
-  });
-}
-
-function writeFooter(paginationEl, config) {
-  const footerSelector = '.js-footer';
-  const footer = paginationEl.find(footerSelector);
-
-  const pageNum = paginationEl.pagination('getSelectedPageNum');
-  const dataPageTotal = paginationEl.pagination('getSelectedPageData').length;
-  const { pageSize } = config;
-  const dataTotal = data.rooms.length;
-
-  const startLimit = (pageNum - 1) * pageSize + 1;
-  const endLimit = (pageNum - 1) * pageSize + dataPageTotal;
-
-  let totalText = '';
-  const dataArr = String(dataTotal).split('');
-  if (Number(dataArr[dataArr.length - 1]) === 1 && dataTotal <= 100) {
-    totalText = `${dataTotal} варианта`;
-  } else {
-    totalText = `${dataTotal} вариантов`;
-  }
-  if (dataTotal > 100) {
-    totalText = '100+ вариантов';
+    this.initPagination();
+    console.log(this);
   }
 
-  const text = `${startLimit} - ${endLimit} из ${totalText} аренды`;
-  footer.html(text);
-}
+  initPagination() {
+    const dataContainer = document.querySelector(
+      this.options.containerSelector
+    );
+    const template = this.template.bind(this);
+    const { event, eventCallback } = this.options;
 
-function initPagination() {
-  const container = $('#js-pagination-container');
-  const footerHtml = '<p class="pagination__footer js-footer">';
-
-  const config = {
-    dataSource: data,
-    locator: 'rooms',
-    autoHidePrevious: true,
-    autoHideNext: true,
-    pageSize: 12,
-    pageRange: 1,
-    callback(rooms) {
-      const html = template(rooms);
-      $('#js-data-container').html(html);
-    },
-    prevText: '<span class="visually-hidden">Назад</span>',
-    nextText: '<span class="visually-hidden">Вперед</span>',
-    footer: footerHtml,
-  };
-
-  if (container.length > 0) {
-    container.pagination(config);
-
-    writeFooter(container, config);
-    initRatings();
-    initSlider();
-
-    container.addHook('afterPaging', () => {
-      initSlider();
-      writeFooter(container, config);
-      initRatings();
+    this.$el.pagination({
+      ...paginationConfig,
+      dataSource: this.options.data,
+      callback(data) {
+        const html = template(data);
+        dataContainer.innerHTML = html;
+      },
+      [event]: eventCallback,
     });
   }
+
+  template(data) {
+    let html = '';
+
+    data.forEach((options) => {
+      const locals = {
+        options,
+      };
+      html += this.options.template(locals);
+    });
+
+    return html;
+  }
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  initPagination();
-});
+export default Pagination;
