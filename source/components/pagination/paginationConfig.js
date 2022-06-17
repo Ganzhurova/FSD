009cb59adc7edfa978c1e9745cec45c41ed83a8d
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 16:35:20 +0800

    Создана конфигурация для paginationjs

---
+++ source/components/pagination/paginationConfig.js
@@ -0,0 +1,11 @@
+const paginationConfig = {
+  autoHidePrevious: true,
+  autoHideNext: true,
+  pageSize: 12,
+  pageRange: 1,
+  prevText: '<span class="visually-hidden">Назад</span>',
+  nextText: '<span class="visually-hidden">Вперед</span>',
+  footer: '<p class="pagination__footer js-footer">',
+};
+
+export default paginationConfig;
