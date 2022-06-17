da8b94d15d8f5b79edb265d05006c378f6be54f8
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 12:03:00 +0800

    Рефакторинг

--- source/components/filter/filter.js
+++ source/components/filter/filter.js
@@ -1,1 +1,11 @@
 import './filter.scss';
+import initBaseDropdowns from '../js/dropdown/initBaseDropdowns';
+import initDropdowns from '../dropdown/initDropdowns';
+
+initBaseDropdowns('.js-filter', {
+  targetSelector: '.js-toggle',
+  closeClass: 'filter--closed',
+  openClass: 'filter--opened',
+});
+
+initDropdowns('.filter .js-dropdown');
