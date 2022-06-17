d3277ba3dd90acb4b8fe89abe1059a9ba2604d6e
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 13:34:07 +0800

    Добавлен вызов initRatings

--- source/pages/UIkit/form-elements/form-elements.js
+++ source/pages/UIkit/form-elements/form-elements.js
@@ -1,3 +1,5 @@
 import initDropdowns from '../../../components/dropdown/initDropdowns';
+import initRatings from '../../../components/rating/initRatings';
 
 initDropdowns('.uikit-page .form-element .js-dropdown');
+initRatings('.uikit-page .form-element .js-rating');
