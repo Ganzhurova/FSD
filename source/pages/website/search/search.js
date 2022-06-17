e3da1f802ba4f22fe924709a3902f076016b5bc5
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 16:38:11 +0800

    Добавлена пагинация на страницу

--- source/pages/website/search/search.js
+++ source/pages/website/search/search.js
@@ -1,4 +1,18 @@
-import './search.scss';
-import data from './data.json';
+import initPaginations from '../../../components/pagination/initPagination';
+import roomTmpl from '../../../components/room/template.pug';
+import initRooms from '../../../components/room/initRooms';
+import roomsData from './rooms-data.json';
 
-export default data;
+import './search.scss';
+
+const paginationOptions = {
+  data: roomsData.rooms,
+  template: roomTmpl,
+  containerSelector: '.js-data-container',
+  event: 'afterPaging',
+  eventCallback: () => {
+    initRooms('.js-data-container .js-room');
+  },
+};
+
+initPaginations('.search__pagination .js-pagination', paginationOptions);
