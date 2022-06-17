8de0a38d9406901afc60222ceb04226bdc64ea4e
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 12:43:11 +0800

    Добаавлена инициализация slickSlider

--- source/pages/UIkit/cards/cards.js
+++ source/pages/UIkit/cards/cards.js
@@ -2,5 +2,8 @@
 
 import 'air-datepicker';
 import baseConfig from '../../../components/calendar/config/baseConfig';
+import initSlickSlider from '../../../components/room/initSlickSlider';
 
 $('.content__section--datepicker .js-datepicker').datepicker(baseConfig);
+
+initSlickSlider('.content__section--cards .js-slider');
