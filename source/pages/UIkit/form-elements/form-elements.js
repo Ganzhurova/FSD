c5441d955d6127aa1d272b46a69fd697885bbfa8
Darima Ganzhurova <DGanzurova@gmail.com>
2022-06-17 16:36:03 +0800

    На страницу добавлена пагинация

--- source/pages/UIkit/form-elements/form-elements.js
+++ source/pages/UIkit/form-elements/form-elements.js
@@ -1,5 +1,7 @@
 import initDropdowns from '../../../components/dropdown/initDropdowns';
 import initRatings from '../../../components/rating/initRatings';
+import initPagination from './initPagination';
 
 initDropdowns('.uikit-page .form-element .js-dropdown');
 initRatings('.uikit-page .form-element .js-rating');
+initPagination('.uikit-page .form-element .js-pagination');
