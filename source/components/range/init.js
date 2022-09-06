1063cc4dd751bd421047158b3990e3f68c5983e6
Darima Ganzhurova <DGanzurova@gmail.com>
2022-09-06 21:10:08 +0800

    Styles of ion-rangeslider isolated #1

--- source/components/range/init.js
+++ source/components/range/init.js
@@ -1,3 +1,4 @@
+import '../../libs/ion-rangeslider/ion-rangeslider.scss';
 import './range.scss';
 import addIonRangeSlider from '../../libs/ion-rangeslider/addIonRangeSlider';
 import config from './config';
