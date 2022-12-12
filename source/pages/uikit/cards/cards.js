18d588d8d867b25f1d9a9c70328a53cb9bb65caa
Darima Ganzhurova <DGanzurova@gmail.com>
2022-12-12 11:44:33 +0800

    #9 cards page refactoring

--- source/pages/uikit/cards/cards.js
+++ source/pages/uikit/cards/cards.js
@@ -1,13 +1,14 @@
 /* global $ */
 
 import 'air-datepicker';
+import './cards.scss';
 
 import baseConfig from '../../../components/calendar/config/baseConfig';
 import initializeReservation from '../../../components/reservation/initializeReservation';
 import initializeRooms from '../../../components/room/initializeRooms';
 import data from '../../website/room-details/data.json';
 
-$('.content__section_datepicker .js-datepicker').datepicker(baseConfig);
+$('.cards__calendar .js-calendar__datepicker').datepicker(baseConfig);
 
 initializeRooms(' .js-room');
-initializeReservation('.content__section_cards .js-reservation', data.info);
+initializeReservation('.cards__reservation .js-reservation', data.info);
